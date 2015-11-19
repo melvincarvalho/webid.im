9c1ddba94f0f1ca60e3d4f15a4af6bc0a2273136
Melvin Carvalho <melvincarvalho@gmail.com>
2015-11-19 19:50:00 +0100

    Add application

--- bin/sendPost.js
+++ bin/sendPost.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 function usage() {
-  console.error('Usage : createPost.js <webid> <message>');
+  console.error('Usage : createPost.js <webid> <message> [application]');
 }
 
 if (!process.argv[2]) {
@@ -18,16 +18,22 @@ if (!process.argv[3]) {
 
 var webid = process.argv[2];
 var message = process.argv[3];
+var application = process.argv[4];
 
-function createPost(webid, message) {
+function createPost(webid, message, application) {
   var turtle;
   turtle += '<#this> ';
   turtle += '    <http://purl.org/dc/terms/created> "'+ new Date().toISOString() +'"^^<http://www.w3.org/2001/XMLSchema#dateTime> ;\n';
   turtle += '    <http://purl.org/dc/terms/creator> <' + webid + '> ;\n';
   turtle += '    <http://rdfs.org/sioc/ns#content> "'+ message.trim() +'" ;\n';
   turtle += '    a <http://rdfs.org/sioc/ns#Post> ;\n';
+
+  if (application) {
+    turtle += '    <https://w3.org/ns/solid/app#application> <' + application + '> ;\n';    
+  }
+
   turtle += '    <http://www.w3.org/ns/mblog#author> <'+ webid +'> .\n';
   return turtle;
 }
 
-console.log(createPost(webid, message));
+console.log(createPost(webid, message, application));
