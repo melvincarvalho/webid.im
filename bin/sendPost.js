32468ab197a574949adac105c6bd81c7a790a96b
Melvin Carvalho <melvincarvalho@gmail.com>
2015-11-19 19:09:46 +0100

    Add sendPost

---
+++ bin/sendPost.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+function usage() {
+  console.error('Usage : createPost.js <webid> <message>');
+}
+
+if (!process.argv[2]) {
+  console.error('Webid is required');
+  usage();
+  process.exit(-1);
+}
+
+if (!process.argv[3]) {
+  console.error('Message is required');
+  usage();
+  process.exit(-1);
+}
+
+var webid = process.argv[2];
+var message = process.argv[3];
+
+function createPost(webid, message) {
+  var turtle;
+  turtle += '<#this> ';
+  turtle += '    <http://purl.org/dc/terms/created> "'+ new Date().toISOString() +'"^^<http://www.w3.org/2001/XMLSchema#dateTime> ;\n';
+  turtle += '    <http://purl.org/dc/terms/creator> <' + webid + '> ;\n';
+  turtle += '    <http://rdfs.org/sioc/ns#content> "'+ message.trim() +'" ;\n';
+  turtle += '    a <http://rdfs.org/sioc/ns#Post> ;\n';
+  turtle += '    <http://www.w3.org/ns/mblog#author> <'+ webid +'> .\n';
+  return turtle;
+}
+
+console.log(createPost(webid, message));
